d535afce7806361ea3df27c4bc6752e7ce8e8354
Afloatz <florentlaot.fr@gmail.com>
2019-02-28 12:40:31 +0100

    PHP project start

---
+++ lib/functions.php
@@ -0,0 +1,16 @@
+<?php
+
+function get_pets()
+{
+    $pdo = new PDO('mysql:dbname=air_pup;host=localhost', 'root', null);
+    $result = $pdo->query('SELECT * FROM pet');
+    $pets = $result->fetchAll();
+
+    return $pets;
+}
+
+function save_pets($petsToSave)
+{
+	$json = json_encode($petsToSave, JSON_PRETTY_PRINT);
+	file_put_contents('data/pets.json', $json);
+}
